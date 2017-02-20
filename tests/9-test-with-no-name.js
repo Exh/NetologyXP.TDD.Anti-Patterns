1c7f42ca62167c649f260b3f441e2f65e695efa8
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-02-21 00:58:16 +0300

    Add 9-test-with-no-name

---
+++ tests/9-test-with-no-name.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var assert = require('chai').assert;
+var Visitor = require('../src/visitor');
+var CocktailBuilder = require('../tests/dsl/cocktail-builder');
+
+suite('US-1/FEAT-23', function () {
+    test('BUG-1234', function () {
+        //
+    });
+});
