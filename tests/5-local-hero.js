81b5a36595be11ce4bdab33f79d81e12ebc688cc
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-02-21 00:50:03 +0300

    Add 5-local-hero

---
+++ tests/5-local-hero.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var assert = require('chai').assert;
+var Visitor = require('../src/visitor');
+var CocktailBuilder = require('../tests/dsl/cocktail-builder');
+
+suite('when drinking cocktail', function () {
+    test('bar is always full of drinks - passes only on my local machine', function () {
+        // ...
+
+        var car = visitor.getMyCar("mycar.jpg");
+        var isOpened = config.getLocalString("bar.isOpened");
+        var userName = "dpavlov";
+
+        // ...
+    });
+});
