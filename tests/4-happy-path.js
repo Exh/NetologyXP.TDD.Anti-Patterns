dd91fc2e357d55e9816aad58e96335d84ea31c56
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-02-21 00:47:38 +0300

    Add 4-happy-path

---
+++ tests/4-happy-path.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var assert = require('chai').assert;
+var Visitor = require('../src/visitor');
+var CocktailBuilder = require('../tests/dsl/cocktail-builder');
+
+suite('when drinking cocktail', function () {
+    test('bar is always full of drinks', function () {
+        //
+    });
+
+    test('barmen is always ready to make a cocktail even if I\'m under 18', function () {
+        //
+    });
+
+    test('visitor becomes more drunk', function () {
+        //
+    });
+
+});
