a4bc4c5841edc5029fa33aec53a683a0e93b2b2d
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-02-21 00:57:03 +0300

    Add 8-silent-catcher

---
+++ tests/8-silent-catcher.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var assert = require('chai').assert;
+var Visitor = require('../src/visitor');
+var CocktailBuilder = require('../tests/dsl/cocktail-builder');
+
+suite('when drinking cocktail', function () {
+    test('should throw general exception', function () {
+        //
+        // throw new BarIsFullException();
+        // test passes even if we throw another exception
+    });
+});
